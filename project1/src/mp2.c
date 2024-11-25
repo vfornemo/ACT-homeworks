1259c01910d95d8ddf30306a84bae860718339f0
Yifan-Jiang2000 <yifan.jiang2000@gmail.com>
2024-11-25 10:07:37 +0000

    implement mp2

---
+++ project1/src/mp2.c
@@ -0,0 +1,36 @@
+#include "mp2.h"
+
+void cal_mp2_correlation(Mol* const m) {
+    m->e_mp2 = 0;
+    for (int32_t i = 0; i < m->n_up; ++i) {
+        for (int32_t j = 0; j < m->n_up; ++j) {
+            for (int32_t a = m->n_up; a < m->mo_num; ++a) {
+                for (int32_t b = a; b < m->mo_num; ++b) {
+                    energy denominator = m->e_mo[i] + m->e_mo[j] - m->e_mo[a] - m->e_mo[b];
+                    if (a == b) {
+                        energy ijab;
+                        if (i > j) {
+                            ijab = get_eri_energy(m, b, a, i, j);
+                        } else {
+                            ijab = get_eri_energy(m, b, a, j, i);
+                        }
+                        m->e_mp2 += ijab * ijab / denominator;
+                        // printf("%f, %f\n", ijab, ijab * ijab / denominator);
+                    } else { 
+                        energy ijab = get_eri_energy(m, b, a, j, i);
+                        energy ijba = get_eri_energy(m, b, a, i, j);
+                        m->e_mp2 += ((ijab * (2 * ijab - ijba)) + (ijba * (2 * ijba - ijab))) / denominator;
+
+                        // printf("%f, %f\n", ijab, ijba);
+                    }
+                    // printf("%f\n", m->e_mp2);
+                    // printf("%f, %f, %f, %f\n", m->e_mo[i],
+                    //                         m->e_mo[j],
+                    //                         m->e_mo[a],
+                    //                         m->e_mo[b]);
+                }
+            }
+        }
+    }
+    return;
+}
---
+++ project1/src/mp2.h
@@ -0,0 +1,8 @@
+#ifndef MP2_H
+#define MP2_H
+
+#include "mol.h"
+
+void cal_mp2_correlation(Mol* const m);
+
+#endif
